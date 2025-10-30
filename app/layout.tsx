86ce6ebfc7989ef53fcf66634c0a1fcaa1fada85
tunahilepayega-ctrl <tunahilepayega9@gmail.com>
2025-10-30 13:13:50 +0530

    Fixed Navbar import path correctly

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
-import Navbar from "../components/Navbar";
+import Navbar from "./components/Navbar";
 import "./globals.css";
-
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
