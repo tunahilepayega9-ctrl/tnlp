232a628cb4c17e9638c19c23f3d067ad8964f2f3
tunahilepayega-ctrl <tunahilepayega9@gmail.com>
2025-10-30 13:22:43 +0530

    Fixed missing Link import

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -13,23 +13,3 @@ export default function Navbar() {
     </nav>
   );
 }
-
-export default function Navbar() {
-  return (
-    <nav style={{
-      background: "linear-gradient(90deg, #1e1e2f, #2d2d5c)",
-      color: "white",
-      padding: "1rem 2rem",
-      display: "flex",
-      justifyContent: "center",
-      gap: "2rem",
-      position: "sticky",
-      top: 0,
-      zIndex: 1000,
-    }}>
-      <Link href="/" style={{ textDecoration: "none", color: "white", fontWeight: 500 }}>Home</Link>
-      <Link href="/about" style={{ textDecoration: "none", color: "white", fontWeight: 500 }}>About</Link>
-      <Link href="/contact" style={{ textDecoration: "none", color: "white", fontWeight: 500 }}>Contact</Link>
-    </nav>
-  )
-}
