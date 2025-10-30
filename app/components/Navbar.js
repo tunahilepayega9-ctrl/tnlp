673772ea3ff3ae340659ddfd8766a4aed5392a5e
tunahilepayega-ctrl <tunahilepayega9@gmail.com>
2025-10-30 13:20:06 +0530

    Fixed missing Link import

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,3 +1,18 @@
+import Link from "next/link";
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo">My Modern Website</div>
+      <ul className="nav-links">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/about">About</Link></li>
+        <li><Link href="/contact">Contact</Link></li>
+      </ul>
+    </nav>
+  );
+}
 
 export default function Navbar() {
   return (
